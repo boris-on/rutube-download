c42b2043659966b489604e2baaf19a071734fc77
mtvy <44533918+mtvy@users.noreply.github.com>
2022-04-25 16:14:03 +0300

    edit code

--- front/routing/front.js
+++ front/routing/front.js
@@ -4,31 +4,14 @@
 
 /**=================================================================*/
           
-import { clearCont } from './js/clear.js';
-
 import { crtFirstFrame, crtFormat,
          crtLoadWrn              } from './js/conf_page.js';
+    
+import { clearCont               } from './js/clear.js';
 
-import { cnvrt_file } from './js/convert.js'; 
+import { cnvrt_file              } from './js/convert.js'; 
 
 /**=================================================================*/
-
-
-/**  @param {int} _quality */
-function rqstServer(_quality)
-{
-    fetch(/* back url ---->*/'', 
-    {
-        method : "post",
-        headers: 
-        {
-            'Accept'      : 'application/json',
-            'Content-Type': 'application/json'
-        },
-        
-        body: JSON.stringify({ quality : _quality})
-    });
-}
 
 
 (function(body, doc) {
@@ -43,8 +26,7 @@ function rqstServer(_quality)
         {
             case 'load_btn':
 
-                
-                fetch(/* back url ---->*/'', 
+                fetch('http://localhost:8000/', 
                     {
                         method : "post",
                         headers: 
@@ -52,25 +34,41 @@ function rqstServer(_quality)
                             'Accept'      : 'application/json',
                             'Content-Type': 'application/json'
                         },
-                        
                         body: JSON.stringify({ link : link.value }) /* <---- video link here */
-                });
+                }).then((response) => {
 
-                if (/* respose */true)
-                {
+                    console.log(response);
+
                     clearCont(body, {'elem' : 'description_panel'});
                     clearCont(body, {'elem' : 'load_btn'});
                     crtFormat(body);
-                }
+                
+                });
+
                 break;
             
             case 'quality_select_1':
 
                 /* request to back server; 240px*/ //rqstServer(240);
 
-                clearCont(body, {'elem' : 'format_selector'});
-                clearCont(body, {'elem' : 'video_preview'});
-                crtLoadWrn(body);
+                fetch('http://localhost:8000/', 
+                {
+                        method : "post",
+                        headers: 
+                        {
+                            'Accept'      : 'application/json',
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({ quality : 240})
+                }).then((response) => {
+
+                    console.log(response);
+
+                    clearCont(body, {'elem' : 'format_selector'});
+                    clearCont(body, {'elem' : 'video_preview'});
+                    crtLoadWrn(body);
+                
+                });
 
                 /* get JSON here ----> */
                 cnvrt_file('1'); /* '1' ----> file name */
